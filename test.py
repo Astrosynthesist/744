5946f90d81f45d0d2a429350462d9ce6fa87dbe3
mwkho <martin.h@hotmail.ca>
2017-10-04 22:04:57 -0400

    Finished code, chatoic movement when t=100

--- test.py
+++ test.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python3
+import numpy as np
+import matplotlib.pyplot as plt
+
 #system parameters
 g=1
 l=1
+w=0.1
+R=0.2
 
+#defining the differential equations
+def y1(t,y):
+    return y
+def y2(t,y):
+    return -(g/l)*np.sin(y)+w**2*(R/l)*np.cos(y-w*t)
+
+# setting time step and integration limits
+start=0
+end=100
+numpoints=1000
+t=np.linspace(start,end,num=numpoints)
+dt = t[1]-t[0]
+
+#setting intial condtions
+(phi0,vel0)=(0,0)
+phi = [phi0]
+vel = [vel0]
+for step in range(numpoints-1):
+    timenow=step*dt
+    vel.append(vel0+ dt*y2(timenow,phi0))
+    phi.append(phi0+dt*y1(timenow,vel0))
+    vel0=vel[step+1]
+    phi0=phi[step+1]
+
+plt.plot(t,phi)
+plt.show()
