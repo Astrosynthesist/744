72e72ab9efca69a326090987c815b03406703027
mwkho <martin.h@hotmail.ca>
2017-10-04 23:31:23 -0400

    refactored code

--- test.py
+++ test.py
@@ -5,32 +5,29 @@ import matplotlib.pyplot as plt
 #system parameters
 g=1
 l=1
-w=0.1
+w=0
 R=0.2
+
+#setting intial condtions
+phi = [0.2]
+vel = [0]
 
 #defining the differential equations
 def y1(t,y):
     return y
 def y2(t,y):
-    return -(g/l)*np.sin(y)+w**2*(R/l)*np.cos(y-w*t)
+    return (w**2)*(R/l)*np.cos(y-w*t)-(g/l)*np.sin(y)
 
 # setting time step and integration limits
 start=0
-end=20
-numPoints=5000
-t=np.linspace(start,end,num=numPoints)
-dt = t[1]-t[0]
+end=100
+numPoints=100000
+t=np.linspace(start, end, num=numPoints)
+dt= t[1]-t[0]
 
-#setting intial condtions
-(phi0,vel0)=(0.2,0)
-phi = [phi0]
-vel = [vel0]
-for step in range(numPoints-1):
+for step in range(0,numPoints-1): # forward Euler's method
     timeNow=step*dt
-    vel.append(vel0+ dt*y2(timeNow,phi0))
-    phi.append(phi0+dt*y1(timeNow,vel0))
-    vel0=vel[step+1]
-    phi0=phi[step+1]
-
+    vel.append(vel[step]+ dt*y2(timeNow,phi[step]))
+    phi.append(phi[step]+dt*y1(timeNow,vel[step]))
 plt.plot(t,phi)
 plt.show()
