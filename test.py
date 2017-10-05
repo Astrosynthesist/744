ed8ae674a622cb3b3d1b8cea9795fac2e8a9c877
mwkho <martin.h@hotmail.ca>
2017-10-04 22:15:47 -0400

    changed initial phi to 0.2 as in Q

--- test.py
+++ test.py
@@ -16,19 +16,19 @@ def y2(t,y):
 
 # setting time step and integration limits
 start=0
-end=100
-numpoints=1000
-t=np.linspace(start,end,num=numpoints)
+end=20
+numPoints=5000
+t=np.linspace(start,end,num=numPoints)
 dt = t[1]-t[0]
 
 #setting intial condtions
-(phi0,vel0)=(0,0)
+(phi0,vel0)=(0.2,0)
 phi = [phi0]
 vel = [vel0]
-for step in range(numpoints-1):
-    timenow=step*dt
-    vel.append(vel0+ dt*y2(timenow,phi0))
-    phi.append(phi0+dt*y1(timenow,vel0))
+for step in range(numPoints-1):
+    timeNow=step*dt
+    vel.append(vel0+ dt*y2(timeNow,phi0))
+    phi.append(phi0+dt*y1(timeNow,vel0))
     vel0=vel[step+1]
     phi0=phi[step+1]
 
