bc83a1f3e33d63f2ea01b5be05a3379e12de0963
Astrosynthesist <32585989+Astrosynthesist@users.noreply.github.com>
2017-10-06 22:37:38 -0400

    Update test.py
    Updated method for determining dt, added formatting to plot.

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 #system parameters
 g=1
 l=1
-w=0
+w=0.1
 R=0.2
 
 #setting intial condtions
@@ -22,12 +22,20 @@ def y2(t,y):
 start=0
 end=100
 numPoints=100000
-t=np.linspace(start, end, num=numPoints)
-dt= t[1]-t[0]
-
+t=[start]
+dt = (end - start)/numPoints
 for i in range(0,numPoints-1): # forward Euler's method
     timeNow=i*dt
     vel.append(vel[i]+ dt*y2(timeNow,phi[i]))
     phi.append(phi[i]+dt*y1(timeNow,vel[i]))
+    t.append(t[i]+dt)
+
+plt.title("Angle of pendulum from vertical")
+plt.xlabel("t (seconds)")
+plt.ylabel(r'$\phi (rad)$')
+plt.minorticks_on()
+plt.grid(b=True, which='major', color='black')
+plt.grid(b=True, which='minor')
+
 plt.plot(t,phi)
 plt.show()
